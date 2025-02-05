e7524f2a31d3b4790456b68753b60852c6541448
Beater24 <gabriel.lopez@ucas-edu.net>
2025-02-04 22:34:11 -0700

    Update practice.py

--- Variables/practice.py
+++ Variables/practice.py
@@ -9,7 +9,7 @@ breakfast = "nothing"
 color = "green"
 school= "Utah County Academy of Sciences"
 year= 2025
-eye_color = "black"
+eye_color = "brown"
 age= 14
 subject= "leaving"
 print("My name is", name, "and I chose the number", number_1_to_10, "along with the number", number_100_to_1000, "I had", breakfast, "for breakfast.", "My favorite color is", color, "I go to", school, "The year is", year, "My eyes are", eye_color, "I am", age, "And my favorite subject is", subject, ".") 
