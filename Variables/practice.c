78f2f50f29ef8ae1b6c3edb8615b9f33589e2d84
Beater24 <gabriel.lopez@ucas-edu.net>
2025-02-04 22:29:57 -0700

    Update practice.c

--- Variables/practice.c
+++ Variables/practice.c
@@ -14,6 +14,6 @@ char subject[] = "leaving";
 
 int main(void){
     
-    printf("Hello my name is %s and I chose the number %d along with the %d", name, num_1_to_10, num_100_to_1000);
+    printf("Hello my name is %s and I chose the number %d along with the number %d, I had %s for breakfast, My favorite color is %s, I go to %s, The year is %d, my eyes are color %s, and I am %d years old, and my favorite subject is %s." name, num_1_to_10, num_100_to_1000, breakfast, color, school, year, eye_color, age, subject);
     return 0;
 }
