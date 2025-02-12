f8c84693d581f57aa34a09380dab0eaec8cf76c8
Beater24 <gabriel.lopez@ucas-edu.net>
2025-02-12 11:49:22 -0700

    calc

--- expressions/financial_calculator.c
+++ expressions/financial_calculator.c
@@ -3,18 +3,33 @@
 #include <math.h>
 
 int main(void){
-    float income;
-    float rent;
-    float utilities;
-    float groceries;
-    float transportation;
-    printf("Welcome to the financial cal");
-    printf("What is your monthly income?");
-    scanf("%f" ,&income);
-    
-
-    float savings = float(income/10);
-    scanf()
-    printf("Hello World");
+    float income, rent, utilities, groceries, transportation, savings, expenses, total; 
+    float iorent, ioutilities, iogroceries, iotransportation, ioexpenses, iosavings;
+    printf("Welcome to the Financial Calculator!\n");
+    printf("How much do you make monthly?");
+    scanf("%f", &income);
+    printf("What is your monthly rent?");
+    scanf("%f", &rent);
+    printf("What is your monthly payment for utilities?");
+    scanf("%f", &utilities);
+    printf("What is your monthly payment for groceries");
+    scanf("%f", &groceries);
+    printf("What is your monthly payment for transportation?");
+    scanf("%f", &transportation);
+    expenses = rent + utilities + groceries + transportation;
+    savings = income * .2;
+    total = income - savings - expenses;
+    printf("Your monthly income is $%.2f\n", income);
+    printf("Your monthly expenses are $%.2f\n", expenses);
+    printf("Your monthly savings is $%.2f\n", expenses);
+    printf("You have $%.2f\n", total);
+    iorent = rent/income *100;
+    ioutilities = utilities/income *100;
+    iogroceries = groceries/income *100;
+    iotransportation = transportation/income *100;
+    ioexpenses = expenses/income *100;
+    iosavings = savings/income *100;
+    printf("Your expenses are %.1f%% of your income\n", ioexpenses);
+    printf("Your savings are %.1f%% of your income\n", iosavings);
     return 0;
 }
---
+++ strings/notes.py
@@ -0,0 +1,10 @@
+# Gabriel Lopez, Strings Notes on Python
+
+#string is a data type that holds any info surrounded by quotation marks "" ''
+
+name = input("What is your first name?\n").strip().capitalize()
+
+print(f"Hello", name, "welcome to my house.")
+
+#when we get syntax erroes it is because we didn't do things correctly. 
+
