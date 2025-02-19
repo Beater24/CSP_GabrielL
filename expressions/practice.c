215362696c087a6e3c21f16f29490580144148c3
Beater24 <gabriel.lopez@ucas-edu.net>
2025-02-19 05:18:09 +0000

    gabi

---
+++ expressions/practice.c
@@ -0,0 +1,9 @@
+// Gabriel Lopez, Expressions Practice on C
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int problem1 = 7-24/8*4+6;
+    printf("%d\n", problem1);
+    return 0;
+}
--- expressions/practice.py
+++ expressions/practice.py
@@ -1,12 +1,21 @@
-#Gabriel Lopez, Practice on Python
+#Gabriel Lopez, Expressions Practice on Python
+
 problem1 =7-24/8*4+6
 problem2 = 18/3-7+2*5
 problem3 = 6*4/12+72/8-9
 problem4 =(17-6/2)+4*3
-problem5 = -2(1*4-2/2)+(6+2-3)
+problem5 = -2*(1*4-2/2)+(6+2-3)
+problem6 = -1*((3-4*7)/5)-2*24/6
+problem7 = (3*5**2/15)-(5-2**2)
+problem8 = (1**4*2**2+3**3)-2**5/4
+problem9 = (22/2-2*5)**2+(4-6/6)**2
 
 print(problem1)
 print(problem2)
 print(problem3)
 print(problem4)
 print(problem5)
+print(problem6)
+print(problem7)
+print(problem8)
+print(problem9)
