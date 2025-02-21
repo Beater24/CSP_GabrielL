626cac94552532184dbde1f5ee1d9d68a007d6be
Beater24 <gabriel.lopez@ucas-edu.net>
2025-02-20 22:59:13 -0700

    Update name_decorator.c

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -3,13 +3,16 @@
 
 int main(void){
     char name[20];
-    char decorator[3];
+    char decorator[1];
+    char opposing_decorator[1]
+    printf("Welcome to the name decorator, This program is made to give your name a little extra touches to make it look better.")
     printf("What is your name?\n");
     scanf("%s", name);
     printf("Choose a symbol for your name to decorate your name.\n");
-    printf("Place the opposing symbol of the symbol you chose earlier\n");
     scanf("%s", decorator);
-    strcat(decorator,decorator,decorator,name,decorator,decorator,decorator)
+    printf("Place the opposing symbol of the symbol you chose earlier (if there isn't a symbol that faces the opposite side just pick the same one you picked earlier"\n");
+    scanf("%s", opposing_decorator);
+    strcat(decorator,decorator,decorator,name,opposing_decorator,opposing_decorator,opposing_decorator)
 
     return 0;
 }
