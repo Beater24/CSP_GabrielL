307780ae7cd76306d94c2430238799b022718ac6
Beater24 <gabriel.lopez@ucas-edu.net>
2025-03-07 11:48:52 -0700

    rej

---
+++ loops/fizz_buzz.c
@@ -0,0 +1,9 @@
+// Gabriel Lopez, FizzBuzz on C
+
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
+
--- loops/fizz_buzz.py
+++ loops/fizz_buzz.py
@@ -1,1 +1,11 @@
 # Gabriel Lopez, FizzBuzz on Python
+
+for x in range(1, 51):  
+    if x % 3 == 0 and x % 5 == 0:  
+        print("FizzBuzz")  
+    elif x % 3 == 0:  
+        print("Fizz")  
+    elif x % 5 == 0:  
+        print("Buzz")  
+    else:  
+        print(x)  
