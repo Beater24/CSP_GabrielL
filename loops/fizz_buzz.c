4c4a2aa5a294a39a9239f4810f155635347b97c1
Beater24 <gabriel.lopez@ucas-edu.net>
2025-03-19 02:16:23 +0000

    brogo

--- loops/fizz_buzz.c
+++ loops/fizz_buzz.c
@@ -1,9 +1,20 @@
 // Gabriel Lopez, FizzBuzz on C
 
 #include <stdio.h>
+#include <math.h>
 
-int main(void){
-    printf("Hello World");
+int main(void) {
+    int q;
+    for (q = 1; q < 51; q++) {
+        if (q % 3 == 0 && q % 5 == 0) {
+            printf("FizzBuzz\n");
+        } else if (q % 3 == 0) {
+            printf("Fizz\n");
+        } else if (q % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", q);
+        }
+    }
     return 0;
 }
-
